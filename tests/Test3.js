4f54b4f8c801acbe8c2f10335204f0fb87c740aa
piyushcoolkarni <piyushcoolkarni@gmail.com>
2015-08-18 12:35:21 +0530

    ip changed

--- tests/Test3.js
+++ tests/Test3.js
@@ -1,7 +1,7 @@
 module.exports = {
   'Demo test Google' : function (browser) {
     browser
-      .url('http://localhost:5050/EmployeeApplicationSprint4-1.0-SNAPSHOT/jsp/add.jsp')
+      .url('http://172.27.59.81:5050/EmployeeApplicationSprint4-1.0-SNAPSHOT/jsp/add.jsp')
       .waitForElementVisible('body', 1000)
 	  .assert.title('Employee Application')
 	  .assert.visible('input')
